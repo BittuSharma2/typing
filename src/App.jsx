38dfac7283033ba7170bcbd5e4386fc586556193
Bittu_Sharma <deepcode143@gmail.com>
2024-10-10 19:29:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>this is typing website</h1>
+        
+    </>
+  )
+}
+
+export default App
